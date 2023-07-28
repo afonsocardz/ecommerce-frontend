cd66d1137a985a15392f9b0e12f671b2b01f15ac
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-07-28 17:58:07 -0300

    feat: add contants for notifications

---
+++ src/constants/notificationMessages.ts
@@ -0,0 +1,10 @@
+export const NOTIFICATION_MESSAGES = {
+  success: {
+    login: 'Login feito com sucesso!',
+    added: 'Produto adicionado com sucesso!',
+    logout: 'Logout feito com sucesso!',
+  },
+  error: {
+    login: 'Senha ou e-mail incorretos!',
+  },
+};
