d8fb2f0aefed38b45c5a6fd52923c4ae84c9375a
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-07-28 17:59:39 -0300

    feat: add logout query and toasters

---
+++ src/app/_hooks/useAuth.ts
@@ -0,0 +1,41 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { authService } from '../_services/authService';
+import { LoginData } from '@/interfaces/authInterface';
+import { toast } from 'react-toastify';
+import { NOTIFICATION_MESSAGES } from '@/constants/notificationMessages';
+import { useRouter } from 'next/navigation';
+
+export default function useAuth() {
+  const { push: navigate } = useRouter();
+  const client = useQueryClient();
+
+  const signInQuery = () =>
+    useMutation({
+      mutationFn: async ({ email, password }: LoginData) =>
+        await authService.signIn(email, password),
+      onSuccess: async () => {
+        await client.invalidateQueries({ queryKey: ['status'] });
+        navigate('/products');
+        toast.success(NOTIFICATION_MESSAGES.success.login);
+      },
+      onError: () => {
+        toast.error(NOTIFICATION_MESSAGES.error.login);
+      },
+    });
+
+  const logoutQuery = () =>
+    useMutation({
+      mutationFn: async () => await authService.logout(),
+      onSuccess: async () => {
+        await client.invalidateQueries({ queryKey: ['status'] });
+        client.clear();
+        navigate('/');
+        toast.success(NOTIFICATION_MESSAGES.success.logout);
+      },
+    });
+
+  return {
+    logoutQuery,
+    signInQuery,
+  };
+}
