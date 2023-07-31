eb154ec03deb10d8a6f42c3514ab5dc53e2b6c6c
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-07-31 14:32:23 -0300

    feat: add cart context

---
+++ src/app/_contexts/CartContext.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { ReactNode, createContext, useContext } from 'react';
+import useCartProducts from '../_hooks/useCartProducts';
+import { UseQueryResult } from '@tanstack/react-query';
+import { CartProductResponseData } from '@/interfaces/cartProductInterface';
+
+interface InitialContext {
+  getCart: UseQueryResult<CartProductResponseData[], unknown>;
+  cartSet: Set<number>;
+}
+
+const CartContext = createContext({} as InitialContext);
+
+export const useCartContext = () => useContext(CartContext);
+
+export default function CartProvider({ children }: { children: ReactNode }) {
+  const { getCartProductsQuery } = useCartProducts();
+  const getCart = getCartProductsQuery();
+
+  const cartSet = new Set(getCart.data?.map((product) => product.productId));
+
+  return (
+    <CartContext.Provider value={{ getCart, cartSet }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
