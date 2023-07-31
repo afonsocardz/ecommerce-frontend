ca8898e52f18814c6e54a48c875c8e0e1412941b
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-07-31 14:39:11 -0300

    refactor: remove interface on service

--- src/app/_services/productsServices.ts
+++ src/app/_services/productsServices.ts
@@ -1,9 +1,9 @@
 import { api } from '@/config/axiosConfig';
-import { ProductsResponse } from '@/interfaces/productInterface';
+
 import { cache } from 'react';
 
 const getAllProducts = cache(async (search: string, page?: number) => {
-  const res = await api.request.get<ProductsResponse>('/products', {
+  const res = await api.request.get('/products', {
     params: {
       search,
       page,
