fdc4051fe986eba1b934704b177782af27c6c921
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-07-31 14:33:19 -0300

    refactor: add type for Product response

--- src/app/_hooks/useProducts.ts
+++ src/app/_hooks/useProducts.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { productsService } from '../_services/productsServices';
+import { ProductsResponse } from '@/interfaces/productInterface';
 
 export default function useProducts() {
   const getAllProducts = (search: string, page?: number) =>
-    useQuery({
+    useQuery<ProductsResponse>({
       queryKey: ['products', { search, page }],
       queryFn: async () => await productsService.getAllProducts(search, page),
     });
