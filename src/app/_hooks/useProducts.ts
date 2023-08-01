51c7a412cf7246e28922d1b2aec21be5afd938e6
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-08-01 14:51:11 -0300

    feat: add initial data to query

--- src/app/_hooks/useProducts.ts
+++ src/app/_hooks/useProducts.ts
@@ -2,12 +2,20 @@ import { useQuery } from '@tanstack/react-query';
 import { productsService } from '../_services/productsServices';
 import { ProductsResponse } from '@/interfaces/productInterface';
 
+const initialProducts: ProductsResponse = {
+  currentPage: 1,
+  totalPages: 1,
+  products: [],
+};
+
 export default function useProducts() {
   const getAllProducts = (search: string, page?: number) =>
     useQuery<ProductsResponse>({
       queryKey: ['products', { search, page }],
       queryFn: async () => await productsService.getAllProducts(search, page),
       suspense: true,
+      initialData: () => initialProducts,
+      staleTime: 0,
     });
 
   return { getAllProducts };
