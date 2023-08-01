78a229cef1ad376e662b6b87a1aa8e73ce757c49
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-08-01 08:06:28 -0300

    refactor: componetizing cart menu structure

--- src/app/components/cart/CartMenu.tsx
+++ src/app/components/cart/CartMenu.tsx
@@ -1,17 +1,16 @@
+'use client';
 import React, { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { DisclosureHook } from '@/app/_hooks/useDisclosure';
 import { useCartContext } from '@/app/_contexts/CartContext';
 import useCartProducts from '@/app/_hooks/useCartProducts';
+import { CartProductResponseData } from '@/interfaces/cartProductInterface';
 
 export default function CartMenu({
   open,
   setOpen,
 }: DisclosureHook): React.ReactElement {
-  const { removeProductQuery } = useCartProducts();
-  const removeProduct = removeProductQuery();
-
   const { getCart } = useCartContext();
   const cart = getCart.data ?? [];
 
@@ -51,7 +50,7 @@ export default function CartMenu({
                     <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                       <div className="flex items-start justify-between">
                         <Dialog.Title className="text-lg font-medium text-gray-900">
-                          Carrinho
+                          {cart.length > 0 ? 'Carrinho' : 'Carrinho vazio'}
                         </Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
                           <button
@@ -64,85 +63,11 @@ export default function CartMenu({
                           </button>
                         </div>
                       </div>
-
-                      <div className="mt-8">
-                        <div className="flow-root">
-                          <ul
-                            role="list"
-                            className="-my-6 divide-y divide-gray-200"
-                          >
-                            {cart.map(({ Product: product, quantity, id }) => (
-                              <li key={product.id} className="flex py-6">
-                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                  <img
-                                    src={product.imageUrl}
-                                    alt={product.description}
-                                    className="h-full w-full object-cover object-center"
-                                  />
-                                </div>
-
-                                <div className="ml-4 flex flex-1 flex-col">
-                                  <div>
-                                    <div className="flex justify-between text-base font-medium text-gray-900">
-                                      <h3>
-                                        <a href="#">{product.name}</a>
-                                      </h3>
-                                      <p className="ml-4">R${product.price}</p>
-                                    </div>
-                                  </div>
-                                  <div className="flex flex-1 items-end justify-between text-sm">
-                                    <p className="text-gray-500">
-                                      X {quantity}
-                                    </p>
-
-                                    <div className="flex">
-                                      <button
-                                        onClick={() => removeProduct.mutate(id)}
-                                        type="button"
-                                        className="font-medium text-blue-600 hover:text-blue-500"
-                                      >
-                                        Remover
-                                      </button>
-                                    </div>
-                                  </div>
-                                </div>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      </div>
+                      <CartProductList {...{ cart }} />
                     </div>
-
-                    <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                      <div className="flex justify-between text-base font-medium text-gray-900">
-                        <p>Subtotal</p>
-                        <p>R${subtotal}</p>
-                      </div>
-                      <p className="mt-0.5 text-sm text-gray-500">
-                        Frete será calculado no checkout
-                      </p>
-                      <div className="mt-6">
-                        <a
-                          href="#"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700"
-                        >
-                          Checkout
-                        </a>
-                      </div>
-                      <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                        <p>
-                          ou{' '}
-                          <button
-                            type="button"
-                            className="font-medium text-blue-600 hover:text-blue-500"
-                            onClick={() => setOpen(false)}
-                          >
-                            Continuar comprando
-                            <span aria-hidden="true"> &rarr;</span>
-                          </button>
-                        </p>
-                      </div>
-                    </div>
+                    {cart.length > 0 && (
+                      <CartCheckoutInfo {...{ subtotal, setOpen }} />
+                    )}
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
@@ -151,5 +76,97 @@ export default function CartMenu({
         </div>
       </Dialog>
     </Transition.Root>
+  );
+}
+
+interface CartProductListProps {
+  cart: CartProductResponseData[];
+}
+
+function CartProductList({ cart }: CartProductListProps) {
+  const { removeProductQuery } = useCartProducts();
+  const removeProduct = removeProductQuery();
+  return (
+    <div className="mt-8">
+      <div className="flow-root">
+        <ul role="list" className="-my-6 divide-y divide-gray-200">
+          {cart.map(({ Product: product, quantity, id }) => (
+            <li key={product.id} className="flex py-6">
+              <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                <img
+                  src={product.imageUrl}
+                  alt={product.description}
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+
+              <div className="ml-4 flex flex-1 flex-col">
+                <div>
+                  <div className="flex justify-between text-base font-medium text-gray-900">
+                    <h3>
+                      <a href="#">{product.name}</a>
+                    </h3>
+                    <p className="ml-4">R${product.price}</p>
+                  </div>
+                </div>
+                <div className="flex flex-1 items-end justify-between text-sm">
+                  <p className="text-gray-500">X {quantity}</p>
+
+                  <div className="flex">
+                    <button
+                      onClick={() => removeProduct.mutate(id)}
+                      type="button"
+                      className="font-medium text-blue-600 hover:text-blue-500"
+                    >
+                      Remover
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+interface CartCheckoutInfoProps {
+  subtotal: number;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function CartCheckoutInfo({ subtotal, setOpen }: CartCheckoutInfoProps) {
+  return (
+    <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+      <div className="flex justify-between text-base font-medium text-gray-900">
+        <p>Subtotal</p>
+        <p>R${subtotal}</p>
+      </div>
+      <p className="mt-0.5 text-sm text-gray-500">
+        Frete será calculado no checkout
+      </p>
+      <div className="mt-6">
+        <a
+          href="#"
+          className="flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700"
+        >
+          Checkout
+        </a>
+      </div>
+      <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+        <p>
+          ou{' '}
+          <button
+            type="button"
+            className="font-medium text-blue-600 hover:text-blue-500"
+            onClick={() => setOpen(false)}
+          >
+            Continuar comprando
+            <span aria-hidden="true"> &rarr;</span>
+          </button>
+        </p>
+      </div>
+    </div>
   );
 }
