386577e05337346fcc4ea88ce5249a2bd896cea4
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-08-07 08:52:24 -0300

    feat: add otimistic request to cart update and remove

--- src/app/_hooks/useCartProducts.ts
+++ src/app/_hooks/useCartProducts.ts
@@ -2,11 +2,15 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import {
   addCartProduct,
+  getCartProduct,
   getCartProducts,
   removeCartProduct,
   updateCartQty,
 } from '../_services/cartProductsService';
-import { AddCartProductBody } from '@/interfaces/cartProductInterface';
+import {
+  AddCartProductBody,
+  CartProductResponseData,
+} from '@/interfaces/cartProductInterface';
 import { NOTIFICATION_MESSAGES } from '@/constants/notificationMessages';
 
 export default function useCartProducts() {
@@ -14,7 +18,18 @@ export default function useCartProducts() {
   const useGetCartProductsQuery = () =>
     useQuery({
       queryKey: ['cart-products'],
-      queryFn: async () => await getCartProducts(),
+      queryFn: async () => {
+        const cartProducts = await getCartProducts();
+
+        cartProducts.forEach((item) => {
+          client.setQueryData<CartProductResponseData>(
+            ['cart-product', item.productId],
+            item
+          );
+        });
+
+        return cartProducts;
+      },
     });
 
   const useAddCartProductQuery = () =>
@@ -29,25 +44,91 @@ export default function useCartProducts() {
 
   const useRemoveProductQuery = () =>
     useMutation({
-      mutationFn: async (cartProductId: number) =>
-        await removeCartProduct(cartProductId),
-      onSuccess: async () => {
-        await client.invalidateQueries({ queryKey: ['cart-products'] });
+      mutationFn: async (productId: number) =>
+        await removeCartProduct(productId),
+      onMutate: (productId) => {
+        const prevData = client.getQueryData<CartProductResponseData>([
+          'cart-product',
+          productId,
+        ]);
+
+        const prevArray = client.getQueryData<CartProductResponseData[]>([
+          'cart-products',
+        ]);
+
+        client.setQueryData(['cart-product', productId], null);
+        client.setQueryData<CartProductResponseData[]>(
+          ['cart-products'],
+          (prev) => {
+            if (prev !== undefined)
+              return prev.filter((item) => item.productId !== productId);
+          }
+        );
+
+        return { prevData, prevArray };
+      },
+      onError: (err, variable, context) => {
+        if (context) {
+          const { prevArray, prevData } = context;
+          if (prevData) {
+            client.setQueryData(['cart-product', prevData.productId], prevData);
+          }
+          if (prevArray) {
+            client.setQueryData<CartProductResponseData[]>(
+              ['cart-products'],
+              prevArray
+            );
+          }
+        }
       },
     });
 
-  const useUpdateProductQtyQuery = (cartId: number) =>
+  const useUpdateProductQtyQuery = (productId: number) =>
     useMutation({
-      mutationFn: async (quantity: number) =>
-        await updateCartQty(quantity, cartId),
-      onSuccess: async () => {
-        await client.invalidateQueries({ queryKey: ['cart-products'] });
+      mutationFn: async (quantity: number) => {
+        updateCartQty(quantity, productId);
+      },
+      onMutate: (quantity: number) => {
+        const previousData = client.getQueryData<CartProductResponseData>([
+          'cart-product',
+          productId,
+        ]);
+
+        const updated = client.setQueryData<CartProductResponseData>(
+          ['cart-product', productId],
+          (prev) => {
+            if (prev) {
+              return {
+                ...prev,
+                quantity,
+                subtotal: prev?.Product.price * quantity,
+              } as CartProductResponseData;
+            }
+          }
+        );
+
+        console.log(updated);
+        return previousData;
+      },
+      onError: (err, variable, context) => {
+        if (context) {
+          client.setQueryData(['cart-product', context.id], context);
+        }
       },
     });
+
+  const useGetCartProduct = (productId: number) =>
+    useQuery({
+      queryKey: ['cart-product', productId],
+      queryFn: async () => await getCartProduct(productId),
+      enabled: false,
+    });
+
   return {
     useGetCartProductsQuery,
     useAddCartProductQuery,
     useRemoveProductQuery,
     useUpdateProductQtyQuery,
+    useGetCartProduct,
   };
 }
